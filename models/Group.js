f0d22a4da873d68fc28eb87e9de7e8d2a6f21337
Sija Joseph <161162142+sijajoseph@users.noreply.github.com>
2025-11-10 20:39:01 +0530

    Update Group.js

--- models/Group.js
+++ models/Group.js
@@ -7,11 +7,3 @@ const GroupSchema = new mongoose.Schema({
 });
 module.exports = mongoose.model('Group', GroupSchema);
 
-// models/Message.js
-const MessageSchema = new mongoose.Schema({
-  group: { type: mongoose.Schema.Types.ObjectId, ref: 'Group' },
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  text: String,
-  createdAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('Message', MessageSchema);
