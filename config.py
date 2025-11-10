5a99203168bcb2d8f2369a1706bd0d5350d341b4
Sija Joseph <161162142+sijajoseph@users.noreply.github.com>
2025-11-10 20:34:15 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "5244716f129015789646146459b3edfebd2605e849aeca80385936e06a227600")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///C:/Users/sijaj/Documents/MyStudyCircleInstance/database.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static', 'uploads')
-    ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
